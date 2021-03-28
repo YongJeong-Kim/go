0a0beb3fb1be00f8458f78f6ffba90a18e0f122e
yongjeong-kim <dydwjd1007>
2021-03-28 11:37:41 +0900

    update

---
+++ gosqlc/db/sqlc/entry_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/yongjeong-kim/go/util"
+)
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+
+	createdEntryId, _ := testQueries.CreateEntry(context.Background(), CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	})
+
+	entryId, _ := createdEntryId.LastInsertId()
+	entry, err := testQueries.GetEntry(context.Background(), entryId)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, account.ID, entry.AccountID)
+	require.Equal(t, entry.Amount, 1111)
+
+	require.NotZero(t, entry.CreatedAt)
+	require.NotZero(t, account.CreatedAt)
+}
