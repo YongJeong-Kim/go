1deeac970d914e703552e5188169cba90d0d6dda
yj.kim@kaistudio.co.kr <yj.kim@kaistudio.co.kr>
2021-08-24 18:27:48 +0900

    update

--- gooauth2/api/user.go
+++ gooauth2/api/user.go
@@ -1,12 +1,52 @@
 package api
 
 import (
+	"context"
 	"gooauth2/token"
+	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-func CreateAuth(payload token.AccessTokenPayload) {
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+}
+
+func Login(c *gin.Context) {
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid JSON provided.")
+		return
+	}
+
+	sampleUser := User{
+		ID:       10,
+		Username: "aaa name",
+	}
+
+	if u.Username != sampleUser.Username || u.ID != sampleUser.ID {
+		c.JSON(http.StatusUnauthorized, "Login failed.")
+		return
+	}
+
+}
+
+func (server *Server) CreateAuth(userID int64, payload *token.AccessTokenPayload) error {
 	at := payload.ExpiredAt
 	rt := payload.RtExpiredAt
 	now := time.Now()
+
+	errAccess := server.redisClient.Set(context.Background(), payload.AccessUUID, strconv.Itoa(int(userID)), at.Sub(now)).Err()
+	if errAccess != nil {
+		return errAccess
+	}
+
+	errRefresh := server.redisClient.Set(context.Background(), payload.RefreshUUID, strconv.Itoa(int(userID)), rt.Sub(now)).Err()
+	if errRefresh != nil {
+		return errRefresh
+	}
+	return nil
 }
