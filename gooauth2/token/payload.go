1bdefd18004b619401cb60b42ff67af00c761b9e
yj.kim@kaistudio.co.kr <yj.kim@kaistudio.co.kr>
2021-08-24 18:59:37 +0900

    update

--- gooauth2/token/payload.go
+++ gooauth2/token/payload.go
@@ -23,21 +23,46 @@ type AccessTokenPayload struct {
 	RtExpiredAt time.Time `json:"rt_expired_at"`
 }
 
-func NewPayload(username string, atDur time.Duration, rtDur time.Duration) (*AccessTokenPayload, error) {
-	tokenID, err := uuid.NewRandom()
+func NewPayload(username string, atDur time.Duration, rtDur time.Duration) (*Payload, error) {
+	atID, err := uuid.NewRandom()
 	if err != nil {
-		return nil, fmt.Errorf("generate token id failed. %s ", err.Error())
+		return nil, fmt.Errorf("generate access token id failed. %s ", err.Error())
 	}
 
-	return &AccessTokenPayload{
-		ID:          tokenID,
-		Username:    username,
-		IssuedAt:    time.Now(),
-		ExpiredAt:   time.Now().Add(atDur),
-		AccessUUID:  uuid.NewString(),
-		RefreshUUID: uuid.NewString(),
-		RtExpiredAt: time.Now().Add(rtDur),
+	rtID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, fmt.Errorf("generate refresh token id failed. %s ", err.Error())
+	}
+
+	refreshUUID := uuid.NewString()
+
+	return &Payload{
+		accessTokenPayload: AccessTokenPayload{
+			ID:          atID,
+			Username:    username,
+			IssuedAt:    time.Now(),
+			ExpiredAt:   time.Now().Add(atDur),
+			AccessUUID:  uuid.NewString(),
+			RefreshUUID: refreshUUID,
+			RtExpiredAt: time.Now().Add(rtDur),
+		},
+		refreshTokenPayload: RefreshTokenPayload{
+			ID:          rtID,
+			Username:    username,
+			IssuedAt:    time.Now(),
+			ExpiredAt:   time.Now().Add(rtDur),
+			RefreshUUID: refreshUUID,
+		},
 	}, nil
+	// return &AccessTokenPayload{
+	// 	ID:          atID,
+	// 	Username:    username,
+	// 	IssuedAt:    time.Now(),
+	// 	ExpiredAt:   time.Now().Add(atDur),
+	// 	AccessUUID:  uuid.NewString(),
+	// 	RefreshUUID: uuid.NewString(),
+	// 	RtExpiredAt: time.Now().Add(rtDur),
+	// }, nil
 }
 
 type RefreshTokenPayload struct {
@@ -49,13 +74,18 @@ type RefreshTokenPayload struct {
 }
 
 type Payload struct {
-	ID          uuid.UUID `json:"id"`
-	Username    string    `json:"username"`
-	IssuedAt    time.Time `json:"issued_at"`
-	ExpiredAt   time.Time `json:"expired_at"`
-	AccessUUID  string    `json:"access_uuid"`
-	RefreshUUID string    `json:"refresh_uuid"`
+	accessTokenPayload  AccessTokenPayload
+	refreshTokenPayload RefreshTokenPayload
 }
+
+// type Payload struct {
+// 	ID          uuid.UUID `json:"id"`
+// 	Username    string    `json:"username"`
+// 	IssuedAt    time.Time `json:"issued_at"`
+// 	ExpiredAt   time.Time `json:"expired_at"`
+// 	AccessUUID  string    `json:"access_uuid"`
+// 	RefreshUUID string    `json:"refresh_uuid"`
+// }
 
 // func NewPayload(username string, duration time.Duration) (*Payload, error) {
 // 	tokenID, err := uuid.NewRandom()
@@ -74,9 +104,9 @@ type Payload struct {
 // 	return payload, nil
 // }
 
-func ExpiredValid(payload Payload) error {
-	if time.Now().After(payload.ExpiredAt) {
-		return ErrExpiredToken
-	}
-	return nil
-}
+// func ExpiredValid(payload Payload) error {
+// 	if time.Now().After(payload.ExpiredAt) {
+// 		return ErrExpiredToken
+// 	}
+// 	return nil
+// }
