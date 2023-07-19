52f9dea5f6234a5fe002bb152cabc842b2628bf9
DT-KAI-YJKIM <hushigine@gmail.com>
2023-07-19 17:49:51 +0900

    add new service

--- gogrpcapi/gapi/server.go
+++ gogrpcapi/gapi/server.go
@@ -2,25 +2,39 @@ package gapi
 
 import (
 	"context"
-	pb "gogrpcapi/pb/user/v1"
+	accountv1 "gogrpcapi/pb/account/v1"
+	userv1 "gogrpcapi/pb/user/v1"
 )
 
 type Server struct {
-	pb.UnimplementedSimpleServerServer
+	UnimplementedServer
+}
+
+type UnimplementedServer struct {
+	accountv1.UnimplementedAccountServiceServer
+	userv1.UnimplementedSimpleServerServer
 }
 
 func NewServer() *Server {
 	return &Server{}
 }
 
-func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
-	return &pb.CreateUserResponse{
+func (server *Server) CreateUser(ctx context.Context, req *userv1.CreateUserRequest) (*userv1.CreateUserResponse, error) {
+	return &userv1.CreateUserResponse{
 		UserId: "vvvv",
 	}, nil
 }
 
-func (server *Server) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
-	return &pb.DeleteUserResponse{
+func (server *Server) DeleteUser(ctx context.Context, req *userv1.DeleteUserRequest) (*userv1.DeleteUserResponse, error) {
+	return &userv1.DeleteUserResponse{
 		UserId: "vdvd",
+	}, nil
+}
+
+func (server *Server) CreateAccount(ctx context.Context, req *accountv1.CreateAccountRequest) (*accountv1.CreateAccountResponse, error) {
+	return &accountv1.CreateAccountResponse{
+		Account: &accountv1.Account{
+			AccountId: "dvdvdv",
+		},
 	}, nil
 }
--- gogrpcapi/main.go
+++ gogrpcapi/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"gogrpcapi/gapi"
+	accountv1 "gogrpcapi/pb/account/v1"
 	userv1 "gogrpcapi/pb/user/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -13,11 +15,32 @@ import (
 	"net/http"
 )
 
+func registerService(grpcServer *grpc.Server, server *gapi.Server) {
+	accountv1.RegisterAccountServiceServer(grpcServer, server)
+	userv1.RegisterSimpleServerServer(grpcServer, server)
+}
+
+func registerHandlerServer(ctx context.Context, grpcMux *runtime.ServeMux, server *gapi.Server) error {
+	register := func(errs ...error) error {
+		for _, err := range errs {
+			if err != nil {
+				return fmt.Errorf("register handler server failed.", err)
+			}
+		}
+
+		return nil
+	}
+
+	return register(
+		userv1.RegisterSimpleServerHandlerServer(ctx, grpcMux, server),
+		accountv1.RegisterAccountServiceHandlerServer(ctx, grpcMux, server))
+}
+
 func main() {
 	server := gapi.NewServer()
 	grpcServer := grpc.NewServer()
 
-	userv1.RegisterSimpleServerServer(grpcServer, server)
+	registerService(grpcServer, server)
 	reflection.Register(grpcServer)
 
 	listener, err := net.Listen("tcp", "0.0.0.0:9090")
@@ -36,9 +59,6 @@ func main() {
 
 func runGatewayServer() {
 	server := gapi.NewServer()
-	//if err != nil {
-	//	log.Fatal().Err(err).Msg("cannot create server")
-	//}
 
 	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
 		MarshalOptions: protojson.MarshalOptions{
@@ -54,36 +74,24 @@ func runGatewayServer() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err := userv1.RegisterSimpleServerHandlerServer(ctx, grpcMux, server)
+	err := registerHandlerServer(ctx, grpcMux, server)
 	if err != nil {
-		log.Fatal("cannot register handler server")
-		//log.Fatal().Err(err).Msg("cannot register handler server")
+		log.Fatal("cannot register handler server", err)
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/api/", grpcMux)
-
-	//statikFS, err := fs.New()
-	//if err != nil {
-	//	log.Fatal().Err(err).Msg("cannot create statik fs")
-	//}
-
-	//swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
-	//mux.Handle("/swagger/", swaggerHandler)
+	mux.Handle("/", grpcMux)
 
 	listener, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
 		log.Fatal("cannot create listener", err)
-		//log.Fatal().Err(err).Msg("cannot create listener")
 	}
 
-	//log.Info().Msgf("start HTTP gateway server at %s", listener.Addr().String())
 	handler := gapi.HTTPLogger(mux)
 	log.Println("start http gateway server at", listener.Addr().String())
 
 	err = http.Serve(listener, handler)
 	if err != nil {
 		log.Fatal("cannot start HTTP gateway server")
-		//log.Fatal().Err(err).Msg("cannot start HTTP gateway server")
 	}
 }
